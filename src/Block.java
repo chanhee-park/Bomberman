af1738c924496f009e30ace31049c00810f73d00
chanhee13p <chanhee13p@gmail.com>
2017-10-29 05:03:53 +0900

    에러수정

--- src/Block.java
+++ src/Block.java
@@ -45,9 +45,9 @@ public class Block {
         double random = Math.random();
 
         if(random<0.4) return null;
-        else if(random<0.6) return new Item(position, Item.Types.SPEED);
-        else if(random<0.8) return new Item(position,Item.Types.POWER);
-        else return new Item(position,Item.Types.NUMBER);
+        else if(random<0.6) return new Item(position, Item.Types.SPEED, img);
+        else if(random<0.8) return new Item(position,Item.Types.POWER, img);
+        else return new Item(position,Item.Types.NUMBER, img);
     }
 
     public void draw(PApplet applet, Types type){
