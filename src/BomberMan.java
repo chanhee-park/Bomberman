aab1a4a24825d588e6613e5004efff48aef8cfb5
chanhee13p <chanhee13p@gmail.com>
2017-10-29 03:46:02 +0900

    아이템 먹는 봄버맨

--- src/BomberMan.java
+++ src/BomberMan.java
@@ -4,7 +4,7 @@ public class BomberMan {
     private Position position;
     Image img;
     private int state = 0;
-    private int dir=0;
+    private int dir = 0;
     private float speed = 0.05f;
     private int power = 1;
     private int numberOfBomb = 1;
@@ -25,7 +25,7 @@ public class BomberMan {
     }
 
     public void setSpeed(float speed) {
-        if(speed>=0.2f) return;
+        if (speed >= 0.2f) return;
         this.speed = speed;
     }
 
@@ -34,7 +34,7 @@ public class BomberMan {
     }
 
     public void setPower(int power) {
-        if(power>=3) return;
+        if (power >= 3) return;
         this.power = power;
     }
 
@@ -43,51 +43,51 @@ public class BomberMan {
     }
 
     public void setNumberOfBomb(int numberOfBomb) {
-        if(numberOfBomb>=3) return;
+        if (numberOfBomb >= 3) return;
         this.numberOfBomb = numberOfBomb;
     }
 
     public void goLeft(Block[][] map) {
         position.setX(position.getX() - speed);
         if (detectCollisionWithBlock(map)) position.setX(position.getX() + speed);
-        dir =15;
-        this.setState(state+1);
+        dir = 15;
+        this.setState(state + 1);
     }
 
     public void goRight(Block[][] map) {
         position.setX(position.getX() + speed);
         if (detectCollisionWithBlock(map)) position.setX(position.getX() - speed);
         dir = 5;
-        this.setState(state+1);
+        this.setState(state + 1);
     }
 
     public void goUP(Block[][] map) {
         position.setY(position.getY() - speed);
         if (detectCollisionWithBlock(map)) position.setY(position.getY() + speed);
         dir = 10;
-        this.setState(state+1);
+        this.setState(state + 1);
     }
 
     public void goDown(Block[][] map) {
         position.setY(position.getY() + speed);
         if (detectCollisionWithBlock(map)) position.setY(position.getY() - speed);
         dir = 0;
-        this.setState(state+1);
+        this.setState(state + 1);
     }
 
-    public boolean isDead(){
+    public boolean isDead() {
         return dead;
     }
-    public void isDead(boolean dead){
+
+    public void isDead(boolean dead) {
         this.dead = dead;
     }
+
     public boolean detectCollisionWithBlock(Block[][] map) {
         int x1 = (int) (position.getX() + 0.1);
         int x2 = (int) (position.getX() + 0.6); // 사이즈 맨 밑에
         int y1 = (int) (position.getY());
         int y2 = (int) (position.getY() + 0.7);
-
-        System.out.println(x1 + " " + x2 + " " + y1 + " " + y2 + " ");
 
         Block.Types brk = Block.Types.BREAKABLE;
         Block.Types unbrk = Block.Types.UNBREAKABLE;
@@ -100,8 +100,8 @@ public class BomberMan {
     }
 
     public void speedUp() {
-        if (speed > 0.8) return;
-        speed += 0.5f;
+        if (speed > 0.2) return;
+        speed += 0.05f;
     }
 
     public void powerUp() {
@@ -120,14 +120,20 @@ public class BomberMan {
 
     public void setState(int state) {
         this.state = state;
-        if(state > 20) {
-            this.state =0;
+        if (state > 20) {
+            this.state = 0;
         }
     }
 
     public void draw(PApplet applet) {
-        applet.image(img.characterMovements[dir+(state/5)],position.getX()*Constants.BLOCK_WIDTH, position.getY()*Constants.BLOCK_HEIGHT);
+        applet.image(img.characterMovements[dir + (state / 5)], position.getX() * Constants.BLOCK_WIDTH, position.getY() * Constants.BLOCK_HEIGHT);
     }
 
+    public void getItem(Item item) {
+        Item.Types itemType = item.getType();
 
+        if (itemType == Item.Types.SPEED) speedUp();
+        if (itemType == Item.Types.POWER) powerUp();
+        if (itemType == Item.Types.NUMBER) numberOfBombUp();
+    }
 }
--- src/Item.java
+++ src/Item.java
@@ -1,14 +1,32 @@
+import processing.core.PApplet;
+
 public class Item {
     Position position;
+
     enum Types {
-        SPEED,
-        POWER,
-        NUMBER,
+        SPEED, POWER, NUMBER
     }
+
     private Types type;
-    Item(Position p, Types t){
+
+    Item(Position p, Types t) {
         position = p;
         type = t;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Types getType() {
+        return type;
+    }
+
+    public void draw(PApplet applet){
+        if(this.type == Types.SPEED) applet.fill(155,0,0);
+        if(this.type == Types.POWER) applet.fill(0,155,0);
+        if(this.type == Types.NUMBER) applet.fill(0,0,155);
+        applet.ellipse(position.getX()*40+10,position.getY()*40+10,25,25);
     }
 }
 
